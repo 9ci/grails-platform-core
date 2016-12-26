210e0fc66265dfaf0424ba18612477e43b36362f
sudhir nimavat <sudhir@nimavat.me>
2016-12-26 22:10:27 +0530

    configure travis

---
+++ travis-build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "### Running plugin tests ###"
+(./gradlew clean check assemble --stacktrace)
+
+if [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_REPO_SLUG == "9ci/grails-platform-core" && $TRAVIS_PULL_REQUEST == 'false' ]]; then
+	echo "### publishing plugin Bintray"
+	./gradlew bintrayUpload
+
+else
+  echo "Not on master branch, so not publishing"
+  echo "TRAVIS_BRANCH: $TRAVIS_BRANCH"
+  echo "TRAVIS_REPO_SLUG: $TRAVIS_REPO_SLUG"
+  echo "TRAVIS_PULL_REQUEST: $TRAVIS_PULL_REQUEST"
+fi
